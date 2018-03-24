3ce078026fc0b35b059666099ac56863d1aad4b6
derrkater <k.m.trojanowski@gmail.com>
2018-03-24 23:15:46 +0100

    Add english data support.
    * Add english survey data.
    * Create panda pipeline for english data in data_preanalysis.

--- src/config.py
+++ src/config.py
@@ -1,5 +1,7 @@
-DATA_PATH = '/Users/derrkater/PycharmProjects/survey_analysis/data'
-OUTPUT_PATH = '/Users/derrkater/PycharmProjects/survey_analysis/output'
+LANGUAGE = 'en'
+
+DATA_PATH = '/Users/derrkater/PycharmProjects/survey_analysis/data/{}'.format(LANGUAGE)
+OUTPUT_PATH = '/Users/derrkater/PycharmProjects/survey_analysis/output/{}'.format(LANGUAGE)
 
 FILTER_OUT_LOW_PHILOSOPHY_KNOWLEDGE = False
 
